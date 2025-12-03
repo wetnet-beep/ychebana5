73414a28f084ef1e183fe6396b98eeba75183b2e
wetnet-beep <avalbaevamirhan3@gmail.com>
2025-12-03 13:03:07 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,9 @@
-// sw.js - простой Service Worker
-const CACHE_NAME = 'uchebana5-v1.0';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/app.js',
-    '/manifest.json'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+// sw.js - пустой Service Worker
+self.addEventListener('install', (event) => {
+    console.log('Service Worker установлен');
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('fetch', (event) => {
+    // Просто пропускаем все запросы
+    event.respondWith(fetch(event.request));
 });
